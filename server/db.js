dd5cde136be9a3a77a9e333a90bb74824ba02207
Sisahga Phimmasone <80082494+Sisahga@users.noreply.github.com>
2024-07-17 16:58:56 -0400

    deploy to heroku

--- server/db.js
+++ server/db.js
@@ -1,12 +1,23 @@
-const Pool = require("pg").Pool;
+// const Pool = require("pg").Pool;
+// require("dotenv").config();
+
+// const pool = new Pool({
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   host: process.env.PG_HOST,
+//   port: process.env.PG_PORT,
+//   database: process.env.PG_DATABASE,
+// });
+
+// module.exports = pool;
+const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // This is necessary to avoid SSL certificate validation errors.
+  },
 });
 
 module.exports = pool;
