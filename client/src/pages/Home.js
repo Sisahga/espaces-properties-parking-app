cf2d43f0fbfad96a455ce7b43f8dd0497ce6ea17
Sisahga Phimmasone <macmee@Sisahgas-MacBook-Pro.local>
2024-10-10 18:38:14 -0400

    fixed prev

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,9 +11,6 @@ const Home = () => {
 
   const authenticated = localStorage.getItem("authenticated");
   var isAdmin = localStorage.getItem("isAdmin");
-  if (isAdmin === null || isAdmin === undefined) {
-    isAdmin = "N";
-  }
   console.log("Authenticated: ", authenticated);
 
   const location = useLocation();
